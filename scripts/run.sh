265af01fc419b4623b75cb945900756c55afbd2f
gumbaba <gumbaba@outlook.com>
2021-05-08 23:38:28 +1000

    updated run.sh with comments and sample_bsnenv with exoort added to the variable to source into env

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,23 +2,19 @@
 set -o pipefail
 
 if [[ `uname` == "Darwin" ]]; then
-    ABSDIR=$(pwd -P)
+    ABSDIR=$( cd "$(dirname "$0")" ; pwd -P )
 else
     ABSPATH=$(readlink -f $0)
     ABSDIR=$(dirname $ABSPATH)
 fi
 
-source $ABSDIR/bsnbot.env
-
-if [[ -z ${IMAGE_VERSION} ]]; then
-    export trader_version="$(curl -s https://api.github.com/repos/bsn-group/trader/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
-    export analyzer_version="$(curl -s https://api.github.com/repos/bsn-group/analyzer/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
-    export ui_version="$(curl -s https://api.github.com/repos/bsn-group/cryptobot-ui/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
-else
-    export trader_version=${IMAGE_VERSION}
-    export analyzer_version=${IMAGE_VERSION}
-    export ui_version=${IMAGE_VERSION}
+if [[   ! -f $ABSDIR/bsnbot.env ]]; then
+    cp $ABSDIR/sample_bsnbot.env $ABSDIR/bsnbot.env
 fi
+
+echo $ABSDIR
+echo "Sourcing bsnbot.env $ABSDIR/bsnbot.env"
+source "$ABSDIR/bsnbot.env"
 
 if [[ -z ${POSTGRES_DB} ]]; then
     echo "db name not found"
@@ -41,23 +37,32 @@ else
     echo "db password found"
 fi
 
-if  [[ $1 == "--live" ]]; then
-    export trader_cli_args="--realorders" # live
-    if [[ -z ${Binance__FuturesKey} ]]; then
-        echo "Binance__FuturesKey is required"
-        exit 1
-    else 
-        echo "Binance__FuturesKey found"
-    fi
-    if [[ -z ${Binance__FuturesSecret} ]]; then
-        echo "Binance__FuturesSecret is required"
-        exit 1
-    else 
-        echo "Binance__FuturesSecret found"
-        echo "Running live"
-    fi
+if [[ -z ${Binance__FuturesKey} ]]; then
+    echo "Binance__FuturesKey is required"
+    exit 1
+else 
+    echo "Binance__FuturesKey found"
+fi
+
+if [[ -z ${Binance__FuturesSecret} ]]; then
+    echo "Binance__FuturesSecret is required"
+    exit 1
+else 
+    echo "Binance__FuturesSecret found"
+fi
+
+if  [[ "${1}" == "--live" ]]; then
+    echo "Running Live"
 else
     echo "Not running live"
+    if [[ -z ${Binance__FuturesUsdtBaseUrl} ]] || [[ -z ${Binance__FuturesCoinMBaseUrl} ]] || [[ -z ${Binance__FuturesWSSUrl} ]]; then
+        echo "Binance testnet details not found"
+        echo 'To run live cmd  "run.sh --live '
+        exit 1
+    else
+        echo  " Binance testnet details found"
+        echo "running in test mode"
+    fi
 fi
 
 if [ -z ${AWS_ACCESS_KEY_ID} ] || [ -z ${AWS_SECRET_ACCESS_KEY} ] || [ -z ${AWS_DEFAULT_REGION} ]; then
@@ -77,6 +82,16 @@ if [[ -z ${SIGNAL_QUEUE} ]]; then
     exit 1
 else
     echo "SIGNAL_QUEUE found"
+fi
+
+if [[ -z ${IMAGE_VERSION} ]]; then
+    export trader_version="$(curl -s https://api.github.com/repos/bsn-group/trader/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
+    export analyzer_version="$(curl -s https://api.github.com/repos/bsn-group/analyzer/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
+    export ui_version="$(curl -s https://api.github.com/repos/bsn-group/cryptobot-ui/branches/main |grep -oP '(?<=(\"sha\"\: \"))[^\"]*' |head -1)"
+else
+    export trader_version=${IMAGE_VERSION}
+    export analyzer_version=${IMAGE_VERSION}
+    export ui_version=${IMAGE_VERSION}
 fi
 
 FILENAME="${ABSDIR}/docker-compose.yml"
